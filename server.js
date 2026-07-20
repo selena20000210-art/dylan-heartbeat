b7995012c796b30ca113dfe45771d47f39eeaa68
selena20000210-art <selena20000210@gmail.com>
2026-07-20 19:03:55 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -1490,6 +1490,7 @@ app.get("/test-bark", async (req, reply) => {
 // 启动服务
 // ========================
 
+const PORT = process.env.PORT || 3000;  // 优先用环境变量，没有就用 3000
 
 app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
